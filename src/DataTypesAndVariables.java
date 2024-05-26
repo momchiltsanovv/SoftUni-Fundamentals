8f6e42e11644067970e5283773f8adfab40db742
Momchil Tsanov <momchil395@gmail.com>
2024-05-26 18:18:07 +0300

    exercises 5 - 9 Data Types

--- src/DataTypesAndVariables.java
+++ src/DataTypesAndVariables.java
@@ -5,7 +5,13 @@ public class DataTypesAndVariables {
         //integerOperations();
         //sumDigits();
         //elevator();
-        sumOfChars();
+        //sumOfChars();
+        //printAsciiTable();
+        //triplesLatinLetters();
+        //waterOverflow();
+        beerKegs();
+
+
 
     }
 
@@ -50,7 +56,7 @@ public class DataTypesAndVariables {
 
         byte lineCount = scanner.nextByte();
         short sum = 0;
-        for (byte i = 0; i < lineCount; i++) {
+        for (byte i = 0; i < lineCount; ++i) {
             char symbol = scanner.next().charAt(0);
             sum += (short) symbol;
         }
@@ -59,6 +65,75 @@ public class DataTypesAndVariables {
 
     }
 
+    public static void printAsciiTable() {
+        Scanner scanner = new Scanner(System.in);
+
+        byte start = scanner.nextByte();
+        byte end = scanner.nextByte();
+
+        for (byte i = start; i <= end; ++i) {
+            System.out.printf(" %c", (char) i);
+        }
+    }
+
+    public static void triplesLatinLetters() {
+        Scanner scanner = new Scanner(System.in);
+
+        byte size = scanner.nextByte();
+        char firstLetter = 'a';
+        char secondLetter = 'b';
+        char thirdLetter = 'c';
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                for (int k = 0; k < size; ++k) {
+                    firstLetter = (char) ('a' + i);
+                    secondLetter = (char) ('a' + j);
+                    thirdLetter = (char) ('a' + k);
+                    System.out.printf("%c%c%c%n", firstLetter, secondLetter, thirdLetter);
+                }
+            }
+        }
+    }
+
+    public static void waterOverflow() {
+        Scanner scanner = new Scanner(System.in);
+
+        short capacity = 255;
+        byte timesPoured = scanner.nextByte();
+
+        short curLiters = 0;
+        for (int i = 0; i < timesPoured; ++i) {
+            short curPoured = scanner.nextShort();
+            curLiters += curPoured;
+            if(curLiters > capacity) {
+                System.out.println("Insufficient capacity!");
+                curLiters -= curPoured;
+            }
+        }
+        System.out.println(curLiters);
+    }
+
+
+    public static void beerKegs() {
+        Scanner scanner = new Scanner(System.in);
+
+        byte inputs = scanner.nextByte();
+        scanner.nextLine();
+        String biggestKeg = "";
+        float maxVolume = 0;
+        for (int i = 0; i < inputs; ++i) {
+            String curKeg = scanner.nextLine();
+            float radius = Float.parseFloat(scanner.nextLine());
+            int height = Integer.parseInt(scanner.nextLine());
+            float curVolume = (float) (Math.PI * radius * radius * height);
+            if(curVolume > maxVolume) {
+                maxVolume = curVolume;
+                biggestKeg = curKeg;
+            }
+            curKeg = "";
+        }
+        System.out.println(biggestKeg);
+    }
 
 }
 
